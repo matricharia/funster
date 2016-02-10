fc3a2e3518a29e97dc5366d7febc65358e3772c3
Laila Rodriguez <matricharia@gmail.com>
2016-02-10 20:58:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funster::Application.config.secret_key_base = '13e12093c0f23aa9ecb23b30cbb736c5232bfd72673edd000d3af9fa86c7cc159ec50c26a7354ca1d82db14ae1b90b7bfaa7b2705f6a6db406f7239d561b8e07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Funster::Application.config.session_store :cookie_store, key: '_funster_session'
